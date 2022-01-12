5e4b39f1f850d54a95752503d79a7aeabab1dc7e
Nicula Ionut <nicula@cobaltbox>
2022-01-12 21:49:19 +0000

    fix logins

--- navbar.php
+++ navbar.php
@@ -9,13 +9,13 @@ session_start();
 printf("<li>%s</li>\n",'<a href="https://cobalt27.xyz">Home</a>');
 if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != 'yes')
 {
-        printf("<li><a href='%s'>%s</a></li>\n", "login.php", "Log in");
-        printf("<li><a href='%s'>%s</a></li>\n", "signup.php", "Sign up");
+        printf("<li><a href='%s'>%s</a></li>\n", "/login.php", "Log in");
+        printf("<li><a href='%s'>%s</a></li>\n", "/signup.php", "Sign up");
 }
 else
 {
         printf("<li>Logged in as: %s</li>\n", $_SESSION['logged_in_as']);
-        printf("<li><a href='%s'>%s</a></li>\n", "logout.php", "Log out");
+        printf("<li><a href='%s'>%s</a></li>\n", "/logout.php", "Log out");
 }
 ?>
 </pre>
